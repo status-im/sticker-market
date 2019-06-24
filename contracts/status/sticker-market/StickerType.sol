b23ef2389d390bc5b1289ab8c63f7760ba7ddf76
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-06-24 04:57:52 -0300

    Change NFT (and SafeMath) to use OpenZeppelin lib

--- contracts/status/sticker-market/StickerType.sol
+++ contracts/status/sticker-market/StickerType.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "../../nft/math/safe-math.sol";
-import "../../nft/tokens/nf-token-enumerable.sol";
+import "../../openzeppelin/math/SafeMath.sol";
+import "../../openzeppelin/token/ERC721/ERC721Enumerable.sol";
 import "../../common/Controlled.sol";
 import "../../common/TokenClaimer.sol";
 
@@ -9,7 +9,7 @@ import "../../common/TokenClaimer.sol";
  * @author Ricardo Guilherme Schmidt (Status Research & Development GmbH)
  * StickerMarket allows any address register "StickerPack" which can be sold to any address in form of "StickerPack", an ERC721 token.
  */
-contract StickerType is Controlled, TokenClaimer, NFTokenEnumerable {
+contract StickerType is Controlled, TokenClaimer, ERC721Enumerable {
     using SafeMath for uint256;
     event Register(uint256 indexed packId, uint256 dataPrice, bytes _contenthash);
     event PriceChanged(uint256 indexed packId, uint256 dataPrice);
